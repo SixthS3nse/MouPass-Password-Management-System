1e10cc44ac425418fc4a184d8acb63cc48c20804
SixthS3nse <68343844+SixthS3nse@users.noreply.github.com>
2023-07-08 19:05:22 +0800

    8/7/2023 7:00PM
    8/7/2023 7:00PM
    (App Fully Functioned)
    Added Export & Import
    
    **Pending**
    Display entry at dashboard bug (CRITICAL)
    Settings (Delete Data)
    Background Service | Notification bar (Safe Browsing API implementation)
    Login & Logout (SharedPrefs)

--- MouPass10/app/src/main/java/com/example/moupass10/SettingsAdapter.java
+++ MouPass10/app/src/main/java/com/example/moupass10/SettingsAdapter.java
@@ -1,14 +1,29 @@
 package com.example.moupass10;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.moupass10.ui.settings.Backup;
@@ -16,6 +31,11 @@ import com.example.moupass10.ui.settings.ChangePassword;
 import com.example.moupass10.ui.settings.ClearData;
 import com.example.moupass10.ui.settings.Restore;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.util.List;
 
 public class SettingsAdapter extends RecyclerView.Adapter<SettingsAdapter.ViewHolder> {
@@ -59,6 +79,8 @@ public class SettingsAdapter extends RecyclerView.Adapter<SettingsAdapter.ViewHo
         return settingsOptions.size();
     }
 
+    //Handle When Each Setting Clicked
+
     private void handleItemClick(int position) {
         // Handle click event here
         // You can perform different actions based on the selected option
@@ -68,12 +90,10 @@ public class SettingsAdapter extends RecyclerView.Adapter<SettingsAdapter.ViewHo
                 context.startActivity(frmChangePassword);
                 break;
             case 1: // Export/Backup
-                Intent frmExportBackup = new Intent(context, Backup.class);
-                context.startActivity(frmExportBackup);
+                exportFiles();
                 break;
             case 2: // Restore
-                Intent frmRestore = new Intent(context, Restore.class);
-                context.startActivity(frmRestore);
+                importFiles();
                 break;
             case 3: // Delete Data
                 Intent frmClearData = new Intent(context, ClearData.class);
@@ -95,4 +115,141 @@ public class SettingsAdapter extends RecyclerView.Adapter<SettingsAdapter.ViewHo
             cardView = itemView.findViewById(R.id.card_view);
         }
     }
+
+    //Export Files - Files will be exported to Downloads folder
+    private void exportFiles() {
+        File exportDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+        File sourceDirectory = new File(context.getFilesDir().getAbsolutePath());
+        File[] files = sourceDirectory.listFiles();
+
+        boolean isContentSnfFound = false;
+        boolean isK3y3SnfFound = false;
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    if (file.getName().equals("content.snf")) {
+                        isContentSnfFound = true;
+                    } else if (file.getName().equals("k3y3.snf")) {
+                        isK3y3SnfFound = true;
+                    }
+                }
+            }
+        }
+
+        if (isContentSnfFound && isK3y3SnfFound) {
+            for (File file : files) {
+                if (file.isFile() && (file.getName().equals("content.snf") || file.getName().equals("k3y3.snf"))) {
+                    try {
+                        exportFile(file, exportDirectory);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            Toast.makeText(context, "Files Exported", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "File Not Found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void exportFile(File file, File exportDirectory) throws IOException {
+        File destinationFile = new File(exportDirectory, file.getName());
+        copyExportFile(file, destinationFile);
+
+        // Scan the file to make it visible in the device's file system
+        MediaScannerConnection.scanFile(context, new String[]{destinationFile.getAbsolutePath()}, null, null);
+    }
+
+    private void copyExportFile(File sourceFile, File destFile) throws IOException {
+        if (!destFile.exists()) {
+            destFile.createNewFile();
+        }
+
+        FileChannel source = null;
+        FileChannel destination = null;
+
+        try {
+            source = new FileInputStream(sourceFile).getChannel();
+            destination = new FileOutputStream(destFile).getChannel();
+            destination.transferFrom(source, 0, source.size());
+        } finally {
+            if (source != null) {
+                source.close();
+            }
+            if (destination != null) {
+                destination.close();
+            }
+        }
+    }
+
+    // Import Files - Files will be imported from Downloads folder
+    private void importFiles() {
+        File importDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+        File[] files = importDirectory.listFiles();
+
+        boolean isContentSnfFound = false;
+        boolean isK3y3SnfFound = false;
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    if (file.getName().equals("content.snf")) {
+                        isContentSnfFound = true;
+                    } else if (file.getName().equals("k3y3.snf")) {
+                        isK3y3SnfFound = true;
+                    }
+                }
+            }
+        }
+
+        if (isContentSnfFound && isK3y3SnfFound) {
+            for (File file : files) {
+                if (file.isFile() && (file.getName().equals("content.snf") || file.getName().equals("k3y3.snf"))) {
+                    try {
+                        importFile(file, importDirectory);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            Toast.makeText(context, "Files Imported", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "File Not Found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void importFile(File file, File importDirectory) throws IOException {
+        importDirectory = new File(context.getFilesDir().getAbsolutePath());
+
+        File destinationFile = new File(importDirectory, file.getName());
+        copyImportFile(file, destinationFile);
+    }
+
+    private void copyImportFile(File sourceFile, File destFile) throws IOException {
+        if (!destFile.exists()) {
+            destFile.createNewFile();
+        }
+
+        FileChannel source = null;
+        FileChannel destination = null;
+
+        try {
+            source = new FileInputStream(sourceFile).getChannel();
+            destination = new FileOutputStream(destFile).getChannel();
+            destination.transferFrom(source, 0, source.size());
+        } finally {
+            if (source != null) {
+                source.close();
+            }
+            if (destination != null) {
+                destination.close();
+            }
+        }
+    }
+
 }
